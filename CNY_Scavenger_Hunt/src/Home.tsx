3594c814a1bbde17c8e5604d1d802f7f556bbb9f
Shanna <shannalee1307@outlook.com>
2026-02-22 19:59:06 +0800

    [MINOR] changed ui to make it look nicer

--- CNY_Scavenger_Hunt/src/Home.tsx
+++ CNY_Scavenger_Hunt/src/Home.tsx
@@ -9,7 +9,7 @@ const Home = (props: Props) => {
     return (
         <div className='m-5'>
             <h1 className='text-5xl '>Scavenger Hunt 2026</h1>
-            <div className='ml-40 mr-40'>
+            <div className='md:ml-40 md:mr-40'>
                 <img src={horseImage} alt="horse" className='h-auto w-full' />
             </div>
             <Leaderboard currentUrl={props.currentUrl}/>
