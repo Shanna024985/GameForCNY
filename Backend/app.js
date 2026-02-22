3a105b4c793db6691209f3f4858a3281f000f031
Shanna <shannalee1307@outlook.com>
2026-02-22 18:54:21 +0800

    [MINOR] trying to debug linking issue causing render not to work

--- Backend/app.js
+++ Backend/app.js
@@ -12,6 +12,11 @@ app.use(express.urlencoded({extended: false}))
 let pathForServingHtmlFile = path.join(__dirname,"../CNY_Scavenger_Hunt/dist")
 console.log(pathForServingHtmlFile)
 app.use("/",express.static(pathForServingHtmlFile))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,"../CNY_Scavenger_Hunt","dist","index.html"));
+});
+  
 let mainRoutes = require("./routers/mainRoutes")
 app.use("/api",mainRoutes)
 
