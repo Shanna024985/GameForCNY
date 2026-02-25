8307970df7a93bfc85d4fe6567cc0357db3e82ce
Shanna <shannalee1307@outlook.com>
2026-02-25 19:17:20 +0800

    [MINOR] added cors

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 let app = express();
 app.use(cors({
-    origin: ["https://google-drive-folder-creation.onrender.com","http://localhost:5173","http://localhost:5174"]
+    origin: ["https://google-drive-folder-creation.onrender.com","http://localhost:5173","http://localhost:5174","https://game-for-cny-server.vercel.app/"]
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
